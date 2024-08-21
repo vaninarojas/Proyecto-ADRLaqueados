ecad4b4fae02a7fd50d11b2a76438a2987dccb1c
vaninarojas <vanina_93@hotmail.com>
2024-08-21 12:10:04 -0300

    proyecto iniciado

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+
+
+function App() {
+
+    useEffect(() => {
+     
+    }, []);
+  
+    
+    return (
+      <BrowserRouter>
+        
+        <Routes>
+        
+        </Routes>
+        
+      </BrowserRouter>
+    );
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,3 @@
+export * from './App';
+export * from './index';
+export * from './main';
